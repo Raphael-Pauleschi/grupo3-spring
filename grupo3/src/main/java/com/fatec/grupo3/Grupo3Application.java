6196cc925c790fde57ad775f40fdf6f5164732f9
dti <dti@LAB204-03>
2022-09-06 11:28:50 -0300

    Login

--- grupo3/src/main/java/com/fatec/grupo3/Grupo3Application.java
+++ grupo3/src/main/java/com/fatec/grupo3/Grupo3Application.java
@@ -2,12 +2,14 @@ package com.fatec.grupo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Grupo3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Grupo3Application.class, args);
+		System.out.println(new BCryptPasswordEncoder().encode("123"));
 	}
 
 }
